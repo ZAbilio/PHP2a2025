b03deff8e9c1b27d138e0742a1826f14f7fbd1e5
enzo abilio <zabilio006@gmail.com>
2026-03-02 21:25:25 -0300

    atividades 1-3

--- aula.php
+++ aula.php
@@ -18,17 +18,39 @@
     }*/
 
 
-    /*atv1*/
+    /*atv1
     echo "atividade 1"
     $num = array (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
     foreach($num= %2==0){
          echo "$num" . "<br>",
-    }
+    }*/
 
-     /*atv2*/
-     $var1=
-     $var2=                                      
-     $var3=
+     /*atv2
+      echo "atividade 2"
+     $var1=readline['var1'];
+     $var2=readline['var2'];                     
+     $var3=readline['var3'];
+       if (var1 > var2 && var1 > var3 ){
+        echo ($var1 +" o primeir valor é o maior entre os tres" );
+       }elseif(var2> var1 && var2> var3){
+         echo ($var2 +" o segundo valor é o maior entre os tres" );
+       }elseif(var3>var1 && var3>var2){
+         echo ($var3 +" o terceiro valor é o maior entre os tres" )
+       }
+       $valormaior = max($var1, $var2, $var3);
+       echo "o maior valor é: "+ $valormaior ."<br>",*/
+       
+       /*atv3*/
+        echo "atividade 3"
+        $num= 1;
+        $tabuada=readline("digite o valor da tabuada:");
+        if($num<=12, $num++){
+        echo ("Tabuada do ".$tabuada)
+        $resultado=($tabuada*$num);
+        echo($tabuada+"x"+$num ": ",+ $resultado);
+        }
+
+
      
 
 ?>
